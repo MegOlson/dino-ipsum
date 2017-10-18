84cebddf572e77e257ff74c9df23dd63504c47d9
Elrey Belmonti and Megan Olson <megan@>
2017-10-18 09:30:18 -0700

    prase response data to be one string instead of array of single word strings

--- js/dino-ipsum-interface.js
+++ js/dino-ipsum-interface.js
@@ -3,9 +3,10 @@ import { DinoIpsum } from './../js/dino-ipsum.js';
 $(document).ready(function(){
   $("#generate").click(function(e){
     e.preventDefault();
-    var api_word = "";
-    var getDinos = $.get('http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=3&words=15').then(function(response){
-      $('.dino-ipsum').text(response[0]);
+    let apiWord = "";
+    let getDinos = $.get('http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=3&words=15').then(function(response){
+      apiWord = response[0].join(" ");
+      $('.dino-ipsum').text(apiWord);
     }).fail(function(error){
       $('.errors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
     });
