ced9ba7a5cca5110e7683dd6737d9ea95b3b3fa3
Elrey Belmonti and Megan Olson <megan@>
2017-10-18 10:33:33 -0700

    start paragraph wit dino ipsum saurus

--- js/dino-ipsum-interface.js
+++ js/dino-ipsum-interface.js
@@ -3,20 +3,27 @@ import { DinoIpsum } from './../js/dino-ipsum.js';
 $(document).ready(function(){
   let apiOutput = "";
   let dinofy = [];
+  let start = "";
   $("#generateParagraph").click(function(e){
     e.preventDefault();
     $("input:checkbox[name=dinofy]:checked").each(function(){
         dinofy.push($(this).val());
     });
     $.get('http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=3&words=50').then(function(response){
-      apiOutput = response[0];
+      apiOutput = response[2];
       if (dinofy.includes("title")) {
         let title = apiOutput.slice(0, 5);
         title = title.join(" ");
         $('.dinoTitle').text(title);
       }
+
       apiOutput = response[0].join(" ");
-      $('.dino-paragraph').text(apiOutput);
+      if (dinofy.includes("start")) {
+        start = "Dino ipsum saurus ";
+        $('.dino-paragraph').text(start + apiOutput);
+      }else{
+        $('.dino-paragraph').text(apiOutput);
+      }
     }).fail(function(error){
       $('.errors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
     });
