3965ea98e256e7f401c6f68100828f2665f3a234
Elrey Belmonti and Megan Olson <elrey@>
2017-10-18 09:57:09 -0700

    add checkboxes for detailed paragraph and push into dinofy array"

--- js/dino-ipsum-interface.js
+++ js/dino-ipsum-interface.js
@@ -2,15 +2,23 @@ import { DinoIpsum } from './../js/dino-ipsum.js';
 
 $(document).ready(function(){
   let apiOutput = "";
+  let dinofy = [];
   $("#generateParagraph").click(function(e){
     e.preventDefault();
+    $("input:checkbox[name=dinofy]:checked").each(function(){
+        dinofy.push($(this).val());
+        console.log(dinofy);
+    });
     $.get('http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=3&words=50').then(function(response){
       apiOutput = response[0].join(" ");
       $('.dino-paragraph').text(apiOutput);
     }).fail(function(error){
       $('.errors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
     });
+
   });
+
+
   $("#generateWord").click(function(e){
     e.preventDefault();
     $.get('http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=3&words=50').then(function(response){
