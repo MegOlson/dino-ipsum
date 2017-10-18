9977015bb2cfc72a3506bee2482ff4a5184fc0b6
Elrey Belmonti and Megan Olson <elrey@>
2017-10-18 09:38:27 -0700

    add button to display dino-ipsum word in addition to paragraph

--- js/dino-ipsum-interface.js
+++ js/dino-ipsum-interface.js
@@ -1,12 +1,21 @@
 import { DinoIpsum } from './../js/dino-ipsum.js';
 
 $(document).ready(function(){
-  $("#generate").click(function(e){
+  let apiOutput = "";
+  $("#generateParagraph").click(function(e){
     e.preventDefault();
-    let apiWord = "";
-    let getDinos = $.get('http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=3&words=15').then(function(response){
-      apiWord = response[0].join(" ");
-      $('.dino-ipsum').text(apiWord);
+    $.get('http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=3&words=50').then(function(response){
+      apiOutput = response[0].join(" ");
+      $('.dino-paragraph').text(apiOutput);
+    }).fail(function(error){
+      $('.errors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
+    });
+  });
+  $("#generateWord").click(function(e){
+    e.preventDefault();
+    $.get('http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=3&words=50').then(function(response){
+      apiOutput = response[0][0];
+      $('.dino-word').text(apiOutput);
     }).fail(function(error){
       $('.errors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
     });
